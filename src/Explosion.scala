0b7cb8a1ae5cc240540fc804eb383b0182e7521e
John Turner <norgg@sylph.(none)>
2008-07-30 00:05:36 +0100

    explosion collision detection

--- src/Explosion.scala
+++ src/Explosion.scala
@@ -1,6 +1,7 @@
 import org.newdawn.slick._
 import sbinary.Instances._
 import sbinary.Operations
+import net.phys2d
 
 class Explosion (var x: Float, var y: Float, var radius: Float, session: Session) {
   val LIFETIME = 10f //second
@@ -9,6 +10,16 @@ class Explosion (var x: Float, var y: Float, var radius: Float, session: Session
   val SOUND = "media/explosion.ogg"
   if (session.isInstanceOf[Client]) {
     new OggPlayer(SOUND).start
+  }
+
+  for (tank <- session.tanks) {
+    val contacts = new Array[phys2d.raw.Contact](1)
+    contacts(0) = new phys2d.raw.Contact
+    val explodeBody = new phys2d.raw.StaticBody(new phys2d.raw.shapes.Circle(radius))
+    explodeBody.setPosition(x, y)
+    if (phys2d.raw.Collide.collide(contacts, explodeBody, tank.body, 0f) > 0) {
+      println("Tank in explosion.")
+    }
   }
     
   def update(delta: Int) {
