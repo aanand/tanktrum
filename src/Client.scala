858ecd947d0f44a658a2bec352938e40e262f479
John Turner <norgg@wyvern.(none)>
2008-10-01 21:01:44 +0100

    Fix ping counter display.

--- src/Client.scala
+++ src/Client.scala
@@ -153,7 +153,6 @@ class Client (hostname: String, port: Int, name: String, container: GameContaine
     }
 
     g.resetTransform
-    g.scale(Main.GAME_WINDOW_RATIO, Main.GAME_WINDOW_RATIO)
     g.setColor(new Color(1f, 1f, 1f))
     g.drawString("Ping: " + latency, 735, 575)
 
