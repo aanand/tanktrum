1d4462450c99ea9c9dce5f775c679fe331b836a9
John Turner <norgg@sylph.(none)>
2009-01-08 14:38:28 +0000

    Fixed resource loading from class path instead of class loader path.

--- src/shared/Resource.scala
+++ src/shared/Resource.scala
@@ -3,11 +3,12 @@ import java.io._
 
 object Resource {
   def get(path: String) = {
+    println("Loading " + path)
     if (new File(path).exists) {
       new FileInputStream(path)
     }
     else {
-      getClass.getResourceAsStream(path)
+      getClass.getClassLoader.getResourceAsStream(path)
     }
   }
 }
