7c8da3b8861da92c528b88ad205ef115149627d0
John Turner <norgg@sylph.(none)>
2009-01-12 13:56:12 +0000

    Shadow offset set to 1.

--- src/client/RichGraphics.scala
+++ src/client/RichGraphics.scala
@@ -3,11 +3,13 @@ package client
 import org.newdawn.slick._
 
 class RichGraphics(g: Graphics) {
+  val shadowOffset = 1
+
   def drawString(string: String, x: Int, y: Int, shadow: Boolean) {
     if (shadow) {
       val color = g.getColor
       g.setColor(new Color(0f, 0f, 0f))
-      g.drawString(string, x+2, y+2)
+      g.drawString(string, x+shadowOffset, y+shadowOffset)
       g.setColor(color)
     }
 
