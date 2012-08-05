bc0818dbcab869ef7b6166c3941f2e733349bc4a
John Turner <norgg@norgg.org>
2012-08-05 18:56:31 +0100

    Advanced AI procedures.

--- src/server/Bot.scala
+++ src/server/Bot.scala
@@ -1,5 +1,20 @@
 package server
 
 class Bot(tank: Tank, playerName: String, playerId: Byte) extends Player(tank, playerName, playerId) {
-  override def timedOut = false
+  val rng = new Random()
+
+  override def timedOut = {
+    //Actions hacked into here because we don't have a player update yet.
+    tank.gun.angle += (rng.nextFloat - 0.5f)
+
+    if (rng.nextFloat < 0.01) tank.thrust = rng.nextInt(3) - 1
+    if (rng.nextFloat < 0.7) tank.gun.firing = false
+    if (rng.nextFloat < 0.001) tank.gun.firing = true
+    
+    if (rng.nextFloat < 0.004) tank.lift = -1
+    if (rng.nextFloat < 0.01)  tank.lift = 0
+
+    false
+  }
+
 }
